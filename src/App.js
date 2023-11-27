d5f782ae319f8b941f379e1e138856b7dd5c937a
bailearay <bailearay1@gmail.com>
2023-11-27 14:20:25 -0500

    starting application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="greet centered top">
+        <h1>Hello</h1>
+      </div>
+      <div className="split right">
+        <div className="info centered">
+          <h2>Our Purpose</h2>
+          <p>Connecting People to People</p>
+        </div>
+      </div>
+      <div className=" split left">
+        <div className="form centered">
+          <h2>Already a member?</h2>
+          <p>Sign in Here</p>
+          <lable htmlFor="email">Your email address: </lable>
+          <input type="email" className="email" id="email"></input>
+          <br />
+          <label htmlFor="pass">Your Password: </label>
+          <input type="password" className="pass" id="pass"></input>
+          <br />
+          <button>Sign In</button>
+          <button>Create Account</button>
+        </div>
+      </div>
     </div>
   );
 }
