693669dac47f45234695ef50dc1e51ebf550ed24
bailearay <bailearay1@gmail.com>
2023-11-28 09:18:19 -0500

    Color scheme changes

--- src/App.js
+++ src/App.js
@@ -3,28 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="greet centered top">
-        <h1>Hello</h1>
-      </div>
-      <div className="split right">
-        <div className="info centered">
-          <h2>Our Purpose</h2>
-          <p>Connecting People to People</p>
-        </div>
-      </div>
-      <div className=" split left">
-        <div className="form centered">
-          <h2>Already a member?</h2>
-          <p>Sign in Here</p>
-          <lable htmlFor="email">Your email address: </lable>
-          <input type="email" className="email" id="email"></input>
-          <br />
-          <label htmlFor="pass">Your Password: </label>
-          <input type="password" className="pass" id="pass"></input>
-          <br />
-          <button>Sign In</button>
-          <button>Create Account</button>
-        </div>
+      <div></div>
+      <div className="greet ">
+        <h1>Welcome to Bailea's Portfolio</h1>
       </div>
     </div>
   );
